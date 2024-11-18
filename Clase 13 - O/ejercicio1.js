864c595463a8a7d8214a476639c2cc0506d0df9f
facuema25 <facuema25@gmail.com>
2024-11-18 14:15:33 -0300

    clase 13

---
+++ Clase 13 - O/ejercicio1.js
@@ -0,0 +1,21 @@
+/*Ejercicio uno*/
+/*Elet nombre = prompt ("Ingrese su nombre");
+let apellido = prompt ("Ingrese su apellido");
+
+let datos = nombre + ` ` + apellido
+
+alert (datos);*/
+
+/*Ejercicio dos*/
+/*let frase = prompt ("Introdoce una frase");
+console.log (frase.length);*/
+
+/*Ejercicio tres*/
+
+let nombre = prompt ("Ingrese su nombre");
+let edad = prompt ("Ingrese su edad")
+
+let datos = nombre + ` ` + edad;
+
+console.log (datos);
+console.log (datos.length);
